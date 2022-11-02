09343c4e3ec3df58b191737dc43c895d614fd7ca
Greyn1 <dheryabengani@outlook.com>
2022-11-02 23:38:38 +0530

    added auth slice in redux

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from './counter/counterSlice';
+import authReducer from './authentication/authSlice';
+import logger from "redux-logger";
 
 export const store = configureStore({
     reducer : {
-        counter : counterReducer
+        counter : counterReducer,
+        auth : authReducer
     },
+    middleware : (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,10 +1,20 @@
 import classes from './Auth.module.css';
+import { authActions } from '../Store/authentication/authSlice';
+import { useDispatch} from 'react-redux';
 
 const Auth = () => {
+  const {login} = authActions;
+  const dispatch = useDispatch();
+
+  const loginHandler = (event) => {
+    event.preventDefault();
+    dispatch(login());
+  }
+  
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={loginHandler}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
             <input type='email' id='email' />
